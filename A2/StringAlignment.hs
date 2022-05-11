0027a82c79aa0f51e69362dc2fbabf20ff9beb82
Alfred Langerbeck <98alla02@gmail.com>
2022-05-11 16:11:53 +0200

    maybe probably don with lab2
    Co-authored-by: maxxjoh <maxxjoh@users.noreply.github.com>

--- A2/StringAlignment.hs
+++ A2/StringAlignment.hs
@@ -1,12 +1,32 @@
+{-
+    Authors: Alfred Langerbeck al5878la-s@student.lu.se
+             Max Johansson ma7580jo-s@student.lu.se
+
+    The hardest part of this assignment was the program optimization. This was because it was very abstract 
+    and hard to understand compared to the rest of the assignment.
+
+    The part we are most proud of was the optAlignment function. 
+-}
+
 -- Hlint options
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
 -- Hlint options
 {-# OPTIONS_GHC -Wno-incomplete-patterns #-}
-import GHC.Base (maxInt, join)
+{-
+    Authors: Alfred Langerbeck al5878la-s@student.lu.se
+             Max Johansson ma7580jo-s@student.lu.se
+-}
+
+-- Hlint options
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+-- Hlint options
+{-# OPTIONS_GHC -Wno-incomplete-patterns #-}
+import GHC.Base (maxInt, join, VecElem (Int16ElemRep))
 import Data.Text.Array (equal)
 import Data.List (intersperse)
 import Distribution.Simple.Setup (ConfigFlags(configConfigureArgs))
 import Control.Concurrent.STM (lengthTBQueue)
+import Distribution.Compat.CharParsing (CharParsing(string))
 --import System.Win32 (COORD(x))
 
 {- Answers to Questions
@@ -25,9 +45,9 @@ string1,string2,string3,string4,string5,string6 :: String
 string1 = "writers"
 string2 = "vintner"
 string3 = "aferociousmonadatemyhamster"
-string4 = "functionalprogrammingrules" 
-string5 = "bananrepubliksinvasionsarmestabsadjutant" 
-string6 = "kontrabasfiolfodralmakarmästarlärling" 
+string4 = "functionalprogrammingrules"
+string5 = "bananrepubliksinvasionsarmestabsadjutant"
+string6 = "kontrabasfiolfodralmakarmästarlärling"
 
 type AlignmentType = (String,String)
 
@@ -42,6 +62,7 @@ similarityScore string1 string2
  | otherwise = scoreMismatch + similarityScore (tail string1) (tail string2)
 
 
+
 {-
 2b: attachesHeads takes two elements and a list of tuples containing two lists
     it attaches h1 and h2 respectively as the head of the list in each tuple
@@ -50,9 +71,6 @@ similarityScore string1 string2
 -}
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
-
-attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
-attachTails h1 h2 aList = [(xs ++ [h1],ys ++ [h2]) | (xs,ys) <- aList]
 
 --2c
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
@@ -66,6 +84,7 @@ optAlignments [] (y:ys) = attachHeads '-' y (optAlignments [] ys)
 optAlignments (x:xs) [] = attachHeads x '-' (optAlignments xs [])
 optAlignments (x:xs) (y:ys) = maximaBy applySimilarityScore (attachHeads x y (optAlignments xs ys) ++ attachHeads '-' y (optAlignments (x:xs) ys) ++ attachHeads x '-' (optAlignments xs (y:ys)))
 
+-- 3 optimizing opt allignment
 optOptAlignments :: String -> String -> [AlignmentType]
 optOptAlignments string1 string2 = snd $ optAlign (length string1) (length string2)
     where
@@ -80,19 +99,38 @@ optOptAlignments string1 string2 = snd $ optAlign (length string1) (length strin
         optEntry 0 j = rest '-' (string2!!(j-1)) $ optAlign 0 (j-1)
         optEntry i 0 = rest (string1!!(i-1)) '-'  $ optAlign (i-1) 0
         optEntry i j = (fst (head restsum),concatMap snd restsum)
-            where 
+            where
                 restsum = maximaBy fst [rest (string1!!(i-1)) (string2!!(j-1)) $ optAlign (i-1) (j-1),rest (string1!!(i-1)) '-' $ optAlign (i-1) j, rest '-' (string2!!(j-1)) $ optAlign i (j-1)]
-         
+
+attachTails :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachTails h1 h2 aList = [(xs ++ [h1],ys ++ [h2]) | (xs,ys) <- aList]
+
+-- 3 optimize similarity score
+similarityScore2 :: String -> String -> Int
+similarityScore2 string1 string2 = score (length string1) (length string2)
+    where
+        score i j = scoreTable!!i!!j
+        scoreTable = [[ scoreEntry i j | j<-[0..]] | i<-[0..] ]
+
+        rest :: Char -> Char -> Int -> Int
+        rest char1 char2 restScore = similarityScore [char1] [char2] + restScore
+
+        scoreEntry :: Int -> Int -> Int
+        scoreEntry 0 0 = 0
+        scoreEntry i 0 = rest (string1!!(i-1)) '-' $ score (i-1) 0
+        scoreEntry 0 j = rest '-' (string2!!(j-1)) $ score 0 (j-1)
+        scoreEntry i j =  maximum [rest (string1!!(i-1)) (string2!!(j-1)) $ score (i-1) (j-1),
+                                   rest (string1!!(i-1)) '-' $ score (i-1) j,
+                                   rest '-' (string2!!(j-1))  $ score i (j-1)
+                                  ]
 
 
 outputOptAlignments :: String -> String -> IO ()
 outputOptAlignments string1 string2 = do
     let optlist = optOptAlignments string1 string2
         listLength = show (length optlist)
-    putStrLn ("There are " ++ listLength ++ " optimal alignments: \n\n" ++ concatMap tupleOutput optlist ++ "There were " ++ listLength ++ " optimal alignments\n")
-
-
-
+        similarity = similarityScore2 string1 string2
+    putStrLn ("There are " ++ listLength ++ " optimal alignments: \n\n" ++ concatMap tupleOutput optlist ++ "There were " ++ listLength ++ " optimal alignments with similarity score: " ++ show similarity ++ "\n")
 
 
 -- Help functions
